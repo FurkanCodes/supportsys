d8a3c748d3e4ac8d084d33bf19f033516c3f006b
Furkan <turkogluvelifurkan@gmail.com>
2023-01-01 22:00:41 +0300

    user registration and login added

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,1 +1,24 @@
-console.log("server...");
+const express = require("express");
+require("dotenv").config();
+const colors = require("colors");
+const app = express();
+
+const port = process.env.PORT || 9000;
+const errorHandler = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+connectDB();
+
+app.get("/api/users", (req, res) => {
+  res.json({ message: "Welcome to the Support Desk" });
+});
+
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
