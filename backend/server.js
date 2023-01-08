75a28dc9fe58940d503ebc8b4c56a81d7b839622
Furkan <turkogluvelifurkan@gmail.com>
2023-01-08 22:00:28 +0300

    render

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,15 @@ app.use(express.urlencoded({ extended: false }));
 
 connectDB();
 
+if (process.env.NODE_ENV === "production") {
+  //*Set static folder up in production
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 app.get("/api/users", (req, res) => {
   res.json({ message: "Welcome to the Support Desk" });
 });
